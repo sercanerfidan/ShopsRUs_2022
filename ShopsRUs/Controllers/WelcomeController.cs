bd99472d546fc149d889adb02bf245209bbc94c2
sercanerfidan <sercan.erfidan@hotmail.com>
2022-01-11 01:09:26 +0300

    some necesssary files

---
+++ ShopsRUs/Controllers/WelcomeController.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ShopsRUs.Domain.Customers;
+using ShopsRUs.Domain.Exceptions;
+using ShopsRUs.Domain.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopsRUs.Controllers
+{
+    [Route("api/[controller]")]
+    public class WelcomeController : ControllerBase
+    {
+        [HttpGet]
+        public string  Get()
+        {
+            return "Welcome to ShopsRUs";
+        }
+    }
+}
---
+++ run.ps1
@@ -0,0 +1,3 @@
+
+    Write-Host "Application starting" -foreground Green
+dotnet run --launch-profile ShopsRUs --project ShopsRUs\ShopsRUsApi.csproj --no-build  
